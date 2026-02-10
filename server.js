50fcdf106a4830d694032a374dafaac28106eff6
Pooja <pooja.dhaundiyal27@gmail.com>
2026-02-11 00:19:14 +0530

    server and ui

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+// Middleware to parse JSON
+app.use(express.json());
+app.use(cors())
+
+// Simple GET route
+// app.get('/', (req, res) => {
+//     res.send('Hello, this is my API!');
+// });
+
+// GET API to return data
+app.get('/users', (req, res) => {
+    const users = [
+        { id: 1, name: 'Pooja' },
+        { id: 2, name: 'Rohit' },
+        { id: 2, name: 'Pankaj' },
+        { id: 2, name: 'Harry' },
+        { id: 2, name: 'Amyra' },
+        { id: 2, name: 'Ankita' },
+    ];
+    res.json(users);
+});
+
+// POST API to receive data
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    res.json({
+        message: 'User received successfully',
+        user: newUser
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+function f1() {
+    return axios.get('/users')
+}
 
 export default function AboutUs() {
+    const { isLoading, data, error, isError, isFetched } = useQuery({
+        queryKey: 'k1',
+        queryFn: f1
+    })
+
     return (
         <div className="about-us">
             <h1>About Us</h1>
             <p>
-                Welcome to our application. We are dedicated to providing the best user 
+                Welcome to our application. We are dedicated to providing the best user
                 experience and innovative solutions.
             </p>
-            
+
             <section className="mission">
                 <h2>Our Mission</h2>
                 <p>
@@ -18,6 +28,17 @@ export default function AboutUs() {
 
             <section className="values">
                 <h2>Our Values</h2>
+                <h2>{isLoading && "Loading..."}</h2>
+                <h2>{isError && "Oops! technical issue"}</h2>
+                <ul>
+                    {data?.data?.map(d => {
+                        return <li>{d.name}</li>
+                    })}
+                </ul>
+            </section>
+
+            <section className="values">
+                <h2>People at work</h2>
                 <ul>
                     <li>Innovation</li>
                     <li>Quality</li>
@@ -32,6 +53,7 @@ export default function AboutUs() {
                     Our talented team is committed to building exceptional products and services.
                 </p>
             </section>
+
         </div>
     );
 }
