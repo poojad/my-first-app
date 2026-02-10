bd040337ee2a72f776064c580da50b0496643abc
Pooja <pooja.dhaundiyal27@gmail.com>
2026-02-10 14:31:01 +0530

    correction

--- src/App.js
+++ src/App.js
@@ -7,27 +7,28 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
+    <Router>
+      <div className="App">
+        {/* <header className="App-header">
         Welcome to my first website - pooja kukshal
       </header> */}
 
-      <nav>
-        {/* <Link to="/">Home</Link> |{" "} */}
-        <a href="/home">Home</a> |{" "}
-        <a href="/about-us">About Us</a> |{" "}
-        <a href="/contact-us">Contact Us</a>
-        {/* <Link to="/user">User</Link> |{" "} */}
-      </nav>
+        <nav>
+          {/* <Link to="/">Home</Link> |{" "} */}
+          <a href="/home">Home</a> |{" "}
+          <a href="/about-us">About Us</a> |{" "}
+          <a href="/contact-us">Contact Us</a>
+          {/* <Link to="/user">User</Link> |{" "} */}
+        </nav>
 
-      <Router>
+
         <Routes>
           <Route path="/" />
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/contact-us" element={<ContactUs />} />
         </Routes>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
