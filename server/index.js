2ab81cdd1cd37798d42da52ac19daad4259bde29
Pooja <pooja.dhaundiyal27@gmail.com>
2026-02-11 15:04:19 +0530

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 import cors from "cors";
 import express from "express";
-// import bodyParser from "bodyParser";
+import { mongoose } from 'mongoose';
 
+// import { MongoClient } from 'mongodb'
+
+// const url = 'mongodb://localhost:27017';
+// const client = new MongoClient(url);
+// const dbName = 'myProject';
+
+// import bodyParser from "bodyParser";
 // const express = require('express');
+
 const app = express();
+mongoose.connect('mongodb://localhost:27017/website')
+
+const PeopleSchema = new mongoose.Schema({
+    name: String
+})
+
+const PeopleModel = mongoose.model("peoples", PeopleSchema)
 
 const port = process.env.PORT || 5000;
 
@@ -36,14 +51,28 @@ const users = [
 
 // GET API to return data
 app.get('/users', (req, res) => {
-    res.json(users);
+    // res.json(users)
+
+    PeopleModel.find({}).then((d) => {
+        res.json(d)
+    }).catch((err) => { console.log(err) })
 });
 
 // POST API to receive data - ex: req will be an object {name: 'pooja'}
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const newUser = req.body.name;
-    users.push({ id: users.length + 1, name: newUser })
-    res.json(users);
+
+    // users.push({ id: users.length + 1, name: newUser })
+    try {
+        const u = await PeopleModel.create({ name: newUser })
+        console.log(u);
+        
+        res.send("User inserted successfully");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Error inserting user");
+    }
+
 });
 
 // Start the server
