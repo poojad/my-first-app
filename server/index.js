e61177dc5431ed81d11ac6963312101a32a3469f
Pooja <pooja.dhaundiyal27@gmail.com>
2026-02-11 20:17:50 +0530

    mongodb configure

--- server/index.js
+++ server/index.js
@@ -11,14 +11,39 @@ import { mongoose } from 'mongoose';
 // import bodyParser from "bodyParser";
 // const express = require('express');
 
+// const MONGODB_CONNECT_URI = "mongodb://localhost:27017/website"
+// const MONGODB_CONNECT_URI = "mongodb+srv://poojad:pooja@11@cluster0.6gs48ad.mongodb.net/website?appName=Cluster0";
+// const MONGODB_CONNECT_URI = "mongodb+srv://poojad:hero1@astroo.xp7fjtg.mongodb.net/Astro?retryWrites=true&w=majority";
+
+const MONGODB_CONNECT_URI = "mongodb+srv://poojad:hero1@astroo.xp7fjtg.mongodb.net/Astro";
+// maybe the password should not be @
+
 const app = express();
-mongoose.connect('mongodb://localhost:27017/website')
+
+const connectDB = () => {
+    console.log("db sucessful")
+
+    mongoose.connect(MONGODB_CONNECT_URI)
+        .then(() => console.log("MongoDB Connected"))
+        .catch((err) => console.log("err"));
+
+    // try {
+    //     mongoose.connect(MONGODB_CONNECT_URI)
+    //     console.log("db sucessful")
+    // } catch (err) {
+    //     console.log("db error")
+    //     console.log(err.message)
+    // }
+}
+
+connectDB();
 
 const PeopleSchema = new mongoose.Schema({
     name: String
 })
 
-const PeopleModel = mongoose.model("peoples", PeopleSchema)
+// const PeopleModel = mongoose.model("peoples", PeopleSchema)
+const PeopleModel = mongoose.model("users", PeopleSchema)
 
 const port = process.env.PORT || 5000;
 
@@ -32,24 +57,24 @@ app.use(
 
 app.use(express.json());
 // app.use(bodyParser.json());
-// // app.use(cors())
+// app.use(cors())
 
 // Simple GET route
-// app.get('/', (req, res) => {
-//     res.send('Hello, this is my API!');
-// });
+app.get('/', (req, res) => {
+    res.send('Hello, this is my API!');
+});
 
-const users = [
-    { id: 1, name: 'Pooja' },
-    { id: 2, name: 'Rohit' },
-    { id: 3, name: 'Pankaj' },
-    { id: 4, name: 'Harry' },
-    { id: 5, name: 'Amyra' },
-    { id: 6, name: 'Ankita' },
-];
+// const users = [
+//     { id: 1, name: 'Pooja' },
+//     { id: 2, name: 'Rohit' },
+//     { id: 3, name: 'Pankaj' },
+//     { id: 4, name: 'Harry' },
+//     { id: 5, name: 'Amyra' },
+//     { id: 6, name: 'Ankita' },
+// ];
 
 
-// GET API to return data
+// // GET API to return data
 app.get('/users', (req, res) => {
     // res.json(users)
 
@@ -58,20 +83,27 @@ app.get('/users', (req, res) => {
     }).catch((err) => { console.log(err) })
 });
 
-// POST API to receive data - ex: req will be an object {name: 'pooja'}
+// // POST API to receive data - ex: req will be an object {name: 'pooja'}
 app.post('/users', async (req, res) => {
     const newUser = req.body.name;
-
-    // users.push({ id: users.length + 1, name: newUser })
-    try {
-        const u = await PeopleModel.create({ name: newUser })
-        console.log(u);
-        
+    await PeopleModel.create({ name: newUser }).then(d => {
         res.send("User inserted successfully");
-    } catch (error) {
-        console.log(error);
+    }).catch(err => {
         res.status(500).send("Error inserting user");
-    }
+    })
+
+    // try {
+    //     // we can save also
+    //     const u = await PeopleModel.create({ name: newUser })
+    //     console.log(u);
+    //     console.log("user-----")
+
+    //     
+    // } catch (error) {
+    //     console.log(error);
+    //     res.status(500).send("Error inserting user");
+    // }
+    // users.push({ id: users.length + 1, name: newUser })
 
 });
 
